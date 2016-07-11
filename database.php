7f7b3be7d657f7ad46be39483a388cacb6969ed7
oakes <oakesonline@gmail.com>
2016-07-11 14:26:41 -0400

    Set character set and PDO object error handling in database.php

--- database.php
+++ database.php
@@ -2,10 +2,9 @@
 
 try {
 		$db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=8889", "root", "root");
-		var_dump($db);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->exec("SET NAMES 'utf8'");
 } catch (Exception $e) {
 	echo "Couldn't connect to the database.";
 	exit;
 }
-
-echo "Connection successful!";
